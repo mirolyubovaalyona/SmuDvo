db5b8247af8579e9cdddab9c2cd1abdf741be208
mirolyubovaalyona <56440546+mirolyubovaalyona@users.noreply.github.com>
2020-01-18 02:02:57 +1000

    бд для голосования
    -------------оно не работает

--- SmuDvo/SmuDvo/settings.py
+++ SmuDvo/SmuDvo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'mainPage.apps.MainPageConfig',
     'account.apps.AccountConfig',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- SmuDvo/polls/models.py
+++ SmuDvo/polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+import datetime
 
-# Create your models here.
+
+class Question(models.Model):
+    poll_title = models.CharField('название опроса', max_length=300)
+    date_published = models.DateField(verbose_name="Дата публикации", default=datetime.datetime.now())
+    is_active = models.BooleanField(verbose_name="Опубликован")
+
+
+class Answer(models.Model):
+    question_id = models.ForeignKey(Question, on_dalete=models.CASCADE, on_delete=models.CASCADE)
+    answer = models.TextField('вариант ответа', default='')
+    votes = models.IntegerField(verbose_name="Голосов", default=0)
+
+
+class Voted(models.Model):
+    question_id = models.ForeignKey(Question, on_dalete=models.CASCADE, on_delete=models.CASCADE)
+    is_voted = models.BooleanField('проголосовал ли пользователь', default=False)
