e1a7ea094a13a56d74af679f5d36968fab3b1b8a
mirolyubovaalyona <56440546+mirolyubovaalyona@users.noreply.github.com>
2020-01-18 07:31:59 +1000

    немного опросов
    в админке есть опросы, надо вывести на сайт их. Спасите

---
+++ SmuDvo/polls/settings.py
@@ -0,0 +1,5 @@
+# Кол-во голосов, при котором пост в админке становится Популярным
+POLLS_POPULAR_VOTES_LIMIT = 100
+
+IMG_TRUE_PATH = '/static/admin/img/icon-yes.gif'
+IMG_FALSE_PATH = '/static/admin/img/icon-no.gif'
