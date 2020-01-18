efa4dbc5a0e8b78ab4f91623890941b331bdd9b8
Edwelde <46779961+Edwelde@users.noreply.github.com>
2020-01-18 10:38:49 +1000

    Valid
    valid )))))0

--- Новая папка/account/forms.py
+++ Новая папка/account/forms.py
@@ -17,11 +17,47 @@ class UserRegistrationForm(forms.ModelForm):
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
 
+    def clean_email(self):
+        cd = self.cleaned_data
+        if len(cd['email']) < 1:
+            raise forms.ValidationError('Enter a valid email address.')
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            raise forms.ValidationError('This email is already used')
+        return cd['email']
+
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
-        return cd['password2']
+        if len(cd['password']) < 6:
+            raise forms.ValidationError('Password must be at least 6 simbols')
+        num = False
+        letr = False
+        LETR = False
+        for ch in cd['password']:
+            if 47 < ord(ch) < 58:
+                num = True
+            if 64 < ord(ch) < 91:
+                LETR = True
+            if 96 < ord(ch) < 123:
+                letr = True
+            if num and letr and LETR:
+                return cd['password2']
+        raise forms.ValidationError('Password must contain at least one of lower- and upper-case letter and a number')
+
+    def clean_first_name(self):
+        cd = self.cleaned_data
+        if not (cd['first_name'].isalpha()):
+            raise forms.ValidationError('Name must contain only letters')
+        return cd['first_name']
+
+    def clean_last_name(self):
+        cd = self.cleaned_data
+        if not (cd['last_name'].isalpha()):
+            raise forms.ValidationError('Name must contain only letters')
+        return cd['last_name']
 
 
 class UserEditForm(forms.ModelForm):
